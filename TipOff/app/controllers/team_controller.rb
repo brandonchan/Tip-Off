6ad46886d16acf620ecac53b21398eacd8bc069c
Brandon <bchansf@gmail.com>
2015-12-03 16:20:17 -0800

    httparty finally works

--- TipOff/app/controllers/team_controller.rb
+++ TipOff/app/controllers/team_controller.rb
@@ -1,2 +1,9 @@
 class TeamController < ApplicationController
+  def index
+    #print out all games coming up
+    #print current record
+    #print game choices
+    response = HTTParty.get("http://api.seatgeek.com/2/events?performers.slug=golden-state-warriors")
+    p response['events'][0]
+  end
 end
